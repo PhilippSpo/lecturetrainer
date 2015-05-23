697421c4466e5f4b4a3cb080e2476d2cc50ce15d
philippspo <philipp.sporrer@planifica.de>
2015-05-23 12:28:21 +0200

    fixes for paginational loading of questions

--- client/templates/questionManagement/questionsList.js
+++ client/templates/questionManagement/questionsList.js
@@ -1,30 +1,43 @@
 Template.questionsList.onCreated(function () {
   var self = this;
+  // initial limit
+  self.loaded = new ReactiveVar(0);
   self.limit = new ReactiveVar(5);
+  // initial ready for inital loding indicator
   self.readyInitial = new ReactiveVar(false);
+  // ready state of subscription
   self.ready = new ReactiveVar(false);
 
   self.autorun(function () {
-    self._subs = [];
+    var limit = self.limit.get();
+    // check if the chapterId is already there
     if (!FlowRouter.getParam('chapterId')) {
       return;
     }
-    var sub = Meteor.subscribe('questionsForChapter', FlowRouter.getParam(
-      'chapterId'), self.limit.get());
-    self._subs.push(sub);
-  });
+    // console.log("Asking for "+limit+" questions");
+    // subscribe to the questions publication
+    var sub = self.subscribe('questionsForChapter', FlowRouter.getParam(
+      'chapterId'), limit);
 
-  self.autorun(function () {
-    var ready = self.ready.get();
-    self._subs.forEach(function (sub) {
-      ready = sub.ready();
-    });
-    if (ready === true && self.readyInitial.get() === false) {
-      self.readyInitial.set(true);
+    if(sub.ready()){
+      // console.log('> Recieved '+limit+ ' questions');
+      self.loaded.set(limit);
+      if(self.readyInitial.get() === false){
+        self.readyInitial.set(true);
+      }
+      self.ready.set(true);
+    } else {
+      // console.log('> sub is not ready yet');
+      self.ready.set(false);
     }
-    self.ready.set(ready);
   });
 
+  // setup the cursor
+  self.questions = function() {
+    return Questions.find({}, {limit: self.loaded.get()});
+  };
+
+  // function that checks if the user scrolled the list to the bottom
   self.checkScrollLimit = function () {
 
     var threshold, target = $("#showMoreResults");
@@ -34,14 +47,16 @@ Template.questionsList.onCreated(function () {
 
     threshold = $('.fixed-content-wrapper').scrollTop() + $(
         '.fixed-content-wrapper').height() - target.height() +
-      100;
+      30;
 
+    // console.log(target.offset().top, threshold);
+    // console.log(target.data("visible"));
     if (target.offset().top < threshold) {
       if (!target.data("visible")) {
         // console.log("target became visible (inside viewable area)");
         target.data("visible", true);
-        if (!this.moreResults()) {
-          this.limit.set(this.limit.get() + 10);
+        if (this.moreResults()) {
+          this.limit.set(this.limit.get() + 1);
         }
       }
     } else {
@@ -51,15 +66,15 @@ Template.questionsList.onCreated(function () {
       }
     }
   };
+  // function to increment limit
   self.incrementLimit = function (inc) {
-    inc = inc || 10;
+    inc = inc || 1;
     self.limit.set(self.limit.get() + inc);
   };
-
+  // check if there are more results
   self.moreResults = function () {
-    return (Questions.find({
-      project: FlowRouter.getParam('projectId')
-    }).count() < self.limit.get());
+    // console.log(self.questions().count(), self.limit.get());
+    return self.questions().count() >= self.limit.get();
   };
 
 });
@@ -70,25 +85,14 @@ Template.questionsList.onRendered(function () {
     self.checkScrollLimit();
   };
   $('.fixed-content-wrapper').scroll(checkScrollLimit);
-  self.checkScrollLimit();
 });
 Template.questionsList.onDestroyed(function () {
-  var self = this;
-  self._subs.forEach(function (sub) {
-    sub.stop();
-  });
-  self._subs = [];
   $('.fixed-content-wrapper').off('scroll');
 });
 
 Template.questionsList.helpers({
   questions: function () {
-    var tmpl = Template.instance();
-    return Questions.find({
-      project: FlowRouter.getParam('projectId')
-    }, {
-      limit: tmpl.limit.get()
-    });
+    return Template.instance().questions();
   },
   projectId: function () {
     return FlowRouter.getParam('projectId');
